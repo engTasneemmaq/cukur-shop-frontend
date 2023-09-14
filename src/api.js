7d1667cbe1579f59a9e4a9285edb91b6b04837a4
Tasneem maqableh <103468147+engTasneemmaq@users.noreply.github.com>
2023-09-14 08:43:53 +0300

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,28 +1,51 @@
-import axios from "axios";
+import React, { useState, useEffect } from 'react';
+
+const [data, setData] = useState(null);
+useEffect(() => {
+     const apiUrl = 'https://api.example.com/data';
+
+     // Fetch data using the fetch API
+     fetch(apiUrl)
+       .then((response) => response.json())
+       .then((result) => {
+         setData(result);
+       })
+       .catch((error) => {
+         console.error('Error fetching data:', error);
+       });
+   }, []); 
+// Empty dependency array means this effect runs once on component mount
 
 
-export const api = async (url, method, token, body, username, password) => {
-  try {
-    let authorization = "";
+// ------------------------------------------------------------------------------
 
-    authorization = `"Bearer " + ${token}`;
 
-    const options = {
-      url: url,
-      method: method,
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json;charset=UTF-8",
-        Authorization: authorization,
-      },
 
-      data: body,
-    };
+// import axios from "axios";
 
-    const response = await axios(options);
+
+// export const api = async (url, method, token, body, username, password) => {
+//   try {
+//     let authorization = "";
+
+//     authorization = `"Bearer " + ${token}`;
+
+//     const options = {
+//       url: url,
+//       method: method,
+//       headers: {
+//         Accept: "application/json",
+//         "Content-Type": "application/json;charset=UTF-8",
+//         Authorization: authorization,
+//       },
+
+//       data: body,
+//     };
+
+//     const response = await axios(options);
   
-    return response.data;
-  } catch (e) {
-    console.log(e);
-  }
-};
+//     return response.data;
+//   } catch (e) {
+//     console.log(e);
+//   }
+// };
